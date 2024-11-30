d5404d43e312636196367afd829b6b4581426bd3
SuhudAlyeqeenDotGithub <alyekeenlearningzone@outlook.com>
2024-11-30 08:14:25 +0000

    Initial Commit,
    Pushing he first app to github

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="bg-green-500 text-white p-4">
+      This My First React App Alhamdullilah.
+    </div>
+  )
+}
+
+export default App
